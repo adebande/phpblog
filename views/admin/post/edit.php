b4bbe115c6a108754666f073695b93c3bd01417a
Aloïs Debande <63793809+adebande@users.noreply.github.com>
2020-11-02 19:16:47 +0100

    Update edit.php and routing

--- views/admin/post/edit.php
+++ views/admin/post/edit.php
@@ -1,1 +1,23 @@
-<h1>Editer l'article <?= $params['id'] ?></h1>
+<?php
+
+use App\PdoConnection;
+use App\Table\PostTable;
+
+$pdo = PdoConnection::getPDO();
+$postTable = new PostTable($pdo);
+$post = $postTable->find($params['id']);
+
+if (!empty($_POST)) {
+    dd('Traiter les données');
+}
+?>
+
+<h1>Editer l'article <?= $post->getName() ?></h1>
+
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="name">Titre</label>
+        <input type="text" class="form-control" name="name" value="<?= $post->getName() ?>">
+    </div>
+    <button class="btn btn-secondary">Modifier</button>
+</form>
